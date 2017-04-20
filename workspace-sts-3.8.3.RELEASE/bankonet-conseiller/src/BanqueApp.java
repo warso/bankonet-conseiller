9b9c417fe926b0cbeb48c9707e8f05bb67435969
Warsama ILMI AMIR <warsama.ilmiamir@gmail.com>
2017-04-20 17:38:52 +0200

    1er commmit

---
+++ workspace-sts-3.8.3.RELEASE/bankonet-conseiller/src/BanqueApp.java
@@ -0,0 +1,12 @@
+
+public class BanqueApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("*****Application Conseiller Bancaire*****");
+		System.out.println("1. Arrêter le programme");
+		System.out.println("2. Ouvrir compte courant");
+		System.out.println("3. Lister tous les clients");
+	}
+
+}
